0efa0d575766b9bd3009c7da993edcfd363f5907
cescobarete <46694783+cescobarete@users.noreply.github.com>
2021-04-26 22:07:59 -0500

    Update

--- final-prod/prod.js
+++ final-prod/prod.js
@@ -1,11 +1,10 @@
-// @ts-check
+// imports
+const express = require('express')
+const prod = express()
+const port = 3000
 
-const config = {
-    endpoint: "<https://manage.documents.azure.com:443/>",
-    key: "<qXbT2LG82V09OhvsqfVNxyaN5hcaR7pA4fyuoJc12IvwZNgUu2EnAee6u5Z6v8aoJtbN3jp8as84TRb0iVMc1A==>",
-    databaseId: "Tasks",
-    containerId: "Items",
-    partitionKey: { kind: "Hash", paths: ["/category"] }
-  };
-  
-  module.exports = config;
+// static Files
+prod.use(express.static('./production'))
+
+// Listeen on port 3000
+prod.listen(port, () => console.info('Listening on port 3000'))
