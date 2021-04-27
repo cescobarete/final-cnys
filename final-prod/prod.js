960ff3b2971f28a1cc7033fcb81554594e97829d
sarah <sarahmahram@lewisu.edu>
2021-04-26 22:43:07 -0500

    Add initial files.

--- final-prod/prod.js
+++ final-prod/prod.js
@@ -1,11 +1,12 @@
-// @ts-check
+const { Router } = require("express");
 
 const config = {
     endpoint: "<https://manage.documents.azure.com:443/>",
     key: "<qXbT2LG82V09OhvsqfVNxyaN5hcaR7pA4fyuoJc12IvwZNgUu2EnAee6u5Z6v8aoJtbN3jp8as84TRb0iVMc1A==>",
-    databaseId: "Tasks",
-    containerId: "Items",
+    databaseId: "customer-db",
+    containerId: "Customer",
     partitionKey: { kind: "Hash", paths: ["/category"] }
   };
   
   module.exports = config;
+
