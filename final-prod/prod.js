fd6f3fdc3a6e1835772fb274c09eaafa706baff2
sarah <sarahmahram@lewisu.edu>
2021-04-26 21:39:52 -0500

    Add initial files.

--- final-prod/prod.js
+++ final-prod/prod.js
@@ -1,10 +1,11 @@
-// imports
-const express = require('express')
-const prod = express()
-const port = 3000
+// @ts-check
 
-// static Files
-prod.use(express.static('./production'))
-
-// Listeen on port 3000
-prod.listen(port, () => console.info('Listening on port 3000'))
+const config = {
+    endpoint: "<https://manage.documents.azure.com:443/>",
+    key: "<qXbT2LG82V09OhvsqfVNxyaN5hcaR7pA4fyuoJc12IvwZNgUu2EnAee6u5Z6v8aoJtbN3jp8as84TRb0iVMc1A==>",
+    databaseId: "Tasks",
+    containerId: "Items",
+    partitionKey: { kind: "Hash", paths: ["/category"] }
+  };
+  
+  module.exports = config;
