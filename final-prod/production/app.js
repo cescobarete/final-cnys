6d1456cedf8dcc1cb082a59efa55b1600eefcb3a
cescobarete <46694783+cescobarete@users.noreply.github.com>
2021-05-01 17:19:39 -0500

    fv

--- final-prod/production/app.js
+++ final-prod/production/app.js
@@ -6,6 +6,7 @@ const dbContext = require("./data/databaseContext");
 const app = express();
 const handlebars = require("express-handlebars")
 const bodyParser = require("body-parser");
+const router = express.Router();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,12 +16,6 @@ app.engine(".handlebars", handlebars({
 }));
 
 app.set("view engine", "handlebars");
-
-// Define new item
-// const newItem = {
-//   id: "1019",
-//   name: "Sama"
-// };
 
 //Async is a utility module which provides straight-forward, 
 //powerful functions for working with asynchronous JavaScript
@@ -35,7 +30,6 @@ async function main(newItem) {
   // Make sure Tasks database is already setup. If not, create it.
   await dbContext.create(client, databaseId, containerId);
   // Create client object database container
-  
   try {
     // <Query items>
     console.log(`Querying container: Items`);
@@ -69,7 +63,7 @@ async function main(newItem) {
      * Pull the id and partition key value from the newly created item.
      * Update the isComplete field to true.
      */
-    const { id, name } = createdItem;
+    /*const { id, name } = createdItem;
 
     const { resource: updatedItem } = await container
       .item(id, name)
@@ -85,8 +79,70 @@ async function main(newItem) {
      */
     //const { resource: result } = await container.item(id, name).delete();
     //console.log(`Deleted item with id: ${id}`);
-    // </DeleteItem>  
+    // </DeleteItem> */
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+
+async function maindelete(newItem) {
+  
+  // Create client object database container
+  const { endpoint, key, databaseId, containerId } = config;
+  const client = new CosmosClient({ endpoint, key });
+  const database = client.database(databaseId);
+  const container = database.container(containerId);
+
+  // Make sure Tasks database is already setup. If not, create it.
+  await dbContext.create(client, databaseId, containerId);
+  // Create client object database container
+  try {
+    // <Query items>
+    console.log(`Querying container: Items`);
+
+    // query to return all items
+    const querySpec = {
+      query: "SELECT * from Customer"
+    };
     
+    // read all items in the Items container
+    const { resources: items } = await container.items
+      .query(querySpec)
+      .fetchAll();
+
+    items.forEach(item => {
+      console.log(`${item.id} - ${item.name}`);
+    });
+    // </QueryItems>
+    
+    // <CreateItem>
+    /** Create new item
+     * newItem is defined at the top of this file
+     */    
+    // </CreateItem>
+    
+    // <UpdateItem>
+    /** Update item
+     * Pull the id and partition key value from the newly created item.
+     * Update the isComplete field to true.
+     */
+    const { id, name } = newItem;
+
+    const { resource: updatedItem } = await container
+      .item(id, name)
+      .replace(newItem);
+
+    console.log(`Updated item: ${updatedItem.id} - ${updatedItem.name}`); 
+    // </UpdateItem>
+    
+    // <DeleteItem>    
+    /**
+     * Delete item
+     * Pass the id and partition key value to delete the item
+     */
+    const { resource: result } = await container.item(id, name).delete();
+    console.log(`Deleted item with id: ${id}`);
+    // </DeleteItem> */
   } catch (err) {
     console.log(err.message);
   }
@@ -97,17 +153,21 @@ app.get("/", (req, res)=> {
 });
 
 app.get("/test/enter", (req, res)=> {
-  // main()
-  // res.render("index", {
-  //   a: newItem.id,
-  //   b: newItem.name
-  // });
   res.sendFile('./index.hsb.html', {root: __dirname})
 });
 
 app.post("/test/enter", async (req, res)=> {
   await main(req.body);
   console.log("Posted!!!!")
+});
+
+app.get("/test/delete", (req, res)=> {
+  res.sendFile('./index.hsb.html', {root: __dirname})
+});
+
+app.post("/test/delete", async (req, res)=> {
+  await maindelete(req.body);
+  console.log("Deleted!!!!")
 });
 
 app.listen(3002, (req, res) => {});
